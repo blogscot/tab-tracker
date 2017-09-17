f4c68241a0b6c884717b0c485c8349bd6c199448
iain <iain.diamond@gmail.com>
2017-09-17 20:45:00 +0100

    Vuetify Registration page.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    <h1 Register User</h1>
-      <input type="email" name="email" v-model="email" placeholder="email" />
-      <br>
-      <input type="password" name="password" v-model="password" placeholder="password" />
-      <br>
-      <div class="error" v-html="error"></div>
-      <button @click="register" type="submit">Register</button>
-
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="theme-color" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" v-model="email" placeholder="email" />
+          <br>
+          <input type="password" name="password" v-model="password" placeholder="password" />
+          <br>
+          <div class="error" v-html="error"></div>
+          <v-btn class="theme-color" @click="register" type="submit">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -40,5 +47,9 @@ export default {
 <style scoped>
 .error {
   color: red;
+}
+
+.theme-color {
+  background-color: #8bf1c6;
 }
 </style>
