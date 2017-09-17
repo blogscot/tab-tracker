a55c448d4372ea7a19536df981068e8bb58ee2d9
iain <iain.diamond@gmail.com>
2017-09-17 19:49:22 +0100

    User registered

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
+const routes = require('./routes.js')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors()) // Enable anyone to access server
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `User ${req.body.email} registered successfully.`,
-  })
-})
+// inject routes
+routes(app)
 
-app.listen(process.env.Port || 8081)
+sequelize.sync().then(() => {
+  app.listen(config.port)
+  console.log(`Server started on port ${config.port}`)
+})
---
+++ server/src/routes.js
@@ -0,0 +1,5 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
+module.exports = app => {
+  app.post('/register', AuthenticationController.register)
+}
