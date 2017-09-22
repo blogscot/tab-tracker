2b32e8ff55eb244a07faddaa2adca2c68e901d8e
iain <iain.diamond@gmail.com>
2017-09-22 20:04:14 +0100

    Remove default hello component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Register from '@/components/Register'
 import Login from '@/components/Login'
 import Songs from '@/components/Songs/Index'
@@ -12,11 +11,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Hello
-    },
     {
       path: '/register',
       name: 'register',
@@ -46,6 +40,10 @@ export default new Router({
       path: '/songs/:songId',
       name: 'song',
       component: ViewSong
+    },
+    {
+      path: '*',
+      redirect: 'songs'
     }
   ]
 })
